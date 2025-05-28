f7db349b03973008797d0d8b631f5573d7cc9e1f
zubry <zubryjs@gmail.com>
2025-05-27 20:38:49 -0400

    Add basic pagination

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,8 +1,23 @@
+import { gt } from "drizzle-orm";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
 
-export async function GET() {
-  const data = await db.select().from(advocates);
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const searchParams = url.searchParams;
 
-  return Response.json({ data });
+  const cursor = parseInt(searchParams.get("cursor") ?? "", 10);
+  const limit = Math.min(parseInt(searchParams.get("limit") ?? "10"), 10);
+
+  let query = db.select().from(advocates).limit(limit).offset(0);
+
+  if (cursor) {
+    query = query.where(cursor ? gt(advocates.id, cursor) : undefined);
+  }
+
+  const data = await query;
+
+  const nextCursor = data.length > 0 ? data[data.length - 1].id : null;
+
+  return Response.json({ data, nextCursor });
 }
